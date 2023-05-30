6f18852616cb94a4f2a53b0696cfdf543c88f0fa
ryanoisin <ryanoisin@users.noreply.github.com>
2023-05-30 14:25:59 +0200

    commit materials

---
+++ setup.R
@@ -0,0 +1,6 @@
+# install packages necessary for the practical
+install.packages("tidyverse")
+install.packages("sandwich")
+install.packages("lmtest")
+install.packages("tidysynth")
+install.packages("fpp3")
